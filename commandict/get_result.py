21b6ed4d4202a0fde238aa9d3e45369110528210
nellaG <seirios0107@gmail.com>
2019-08-25 18:44:47 +0900

    use argv to get the keyword

--- commandict/get_result.py
+++ commandict/get_result.py
@@ -1,5 +1,7 @@
 """ get_result.py """
 
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,17 +10,18 @@ DAUM_DICT_HOST = "https://alldic.daum.net/"
 
 # TODO: 키워드 입력받기
 # TODO: 설정에서 언어 입력받고 설정 저장할 수 있게
-KEYWORD = 'test'
 LANG = 'eng'
 
 
 def parse(html: str):
     bs = BeautifulSoup(html, 'html.parser')
-    return bs.findAll('ul', attrs={'class': 'list_search'})[0].text
+    return bs.findAll('meta',
+                      attrs={'property': 'og:description'})[0].get('content')
 
 
 if __name__ == "__main__":
-    url = f'{DAUM_DICT_HOST}/search.do?q={KEYWORD}&dic={LANG}'
+    KEYWORD = sys.argv[1]
+    url = f'{DAUM_DICT_HOST}search.do?q={KEYWORD}&dic={LANG}'
     response = requests.get(url)
     meanings = parse(response.text)
     print(meanings)
